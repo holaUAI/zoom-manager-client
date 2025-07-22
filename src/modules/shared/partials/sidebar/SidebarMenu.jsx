bf29d8101434130ff992b72cfa645d30b3972875
holaUAI <hola@uai.edu.pe>
2025-07-22 14:15:45 +0000

    style: 💄 cambios en nombramiento

--- src/modules/shared/partials/sidebar/SidebarMenu.jsx
+++ src/modules/shared/partials/sidebar/SidebarMenu.jsx
@@ -5,8 +5,8 @@ import { Link } from "react-router-dom";
 export default function SidebarMenu({ open, isActive }) {
     const links = [
       { to: "/", label: "Dashboard", icon: <Home /> },
-      { to: "/reuniones", label: "Reuniones", icon: <Video /> },
-      { to: "/docentes", label: "Docentes", icon: <Users /> },
+      { to: "/reuniones", label: "Clases", icon: <Video /> },
+      { to: "/docentes", label: "Profesores", icon: <Users /> },
       { to: "/cursos", label: "Cursos", icon: <BookOpen /> },
     ];
   
